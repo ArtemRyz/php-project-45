991a6f3431784ed887e543bb7af8d56f29adf43b
ArtemRyz <RyzArtem@yandex.ru>
2023-07-16 00:23:05 +0500

    add src/Games/Calc.php and src/Games/Even.php

---
+++ src/Games/Calc.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace BrainGames\Calc;
+
+use function BrainGames\Engine\runGame;
+
+const GAMEINFO = 'What is the result of the expression?';
+
+function runCalc()
+{
+    $round = 3;
+    $i = 0;
+    $arrQuestion = [];
+    $arrAnswer = [];
+    while ($i < $round) {
+        $num1 = rand(1, 10);
+        $num2 = rand(1, 10);
+        $operator = randomOperator();
+        $question = "{$num1} {$operator} {$num2}";
+        $correctAnswer = result($num1, $operator, $num2);
+        $arrQuestion[] = $question;
+        $arrAnswer[] = $correctAnswer;
+        $i++;
+    }
+    runGame(GAMEINFO, $arrQuestion, $arrAnswer);
+}
+
+
+function randomOperator()
+{
+    $randomNum = rand(1, 3);
+    if ($randomNum === 1) {
+        $randomOperator = "+";
+    } elseif ($randomNum === 2) {
+        $randomOperator = "-";
+    } elseif ($randomNum === 3) {
+        $randomOperator = "*";
+    }
+    return $randomOperator;
+}
+
+function result($number1, $operation, $number2)
+{
+    switch ($operation) {
+        case "+":
+            $correctResult = $number1 + $number2;
+            break;
+        case "-":
+            $correctResult = $number1 - $number2;
+            break;
+        case "*":
+            $correctResult = $number1 * $number2;
+            break;
+    }
+    return $correctResult;
+}
