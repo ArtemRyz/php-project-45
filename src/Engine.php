dca2ab4bfc451b35992c341dc17dbc1d975a4459
ArtemRyz <RyzArtem@yandex.ru>
2023-07-23 13:11:16 +0500

    update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -4,27 +4,25 @@ namespace BrainGames\Engine;
 
 use function cli\line;
 use function cli\prompt;
+use function BrainGames\Cli\greeting;
 
-function runGame($info, $gameQuestion, $gameResult)
+function runGame(string $info, $gameQuestion, $gameResult)
 {
-    line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    $name = greeting();
     line($info);
 
     $rounds = 3;
+
     for ($i = 0; $i < $rounds; $i++) {
-        $Question = line("Question: %s", $gameQuestion[$i]);
+        line("Question: %s", $gameQuestion[$i]);
         $userAnswer = prompt('Your answer');
         if ($userAnswer == $gameResult[$i]) {
             line('Correct!');
         } elseif ($userAnswer != $gameResult[$i]) {
             line("'%s' is wrong answer ;(. Correct answer was '%s'.", $userAnswer, $gameResult[$i]);
             line("Let's try again, %s!", $name);
-            break;
-        }
-        if ($i === 2) {
-            line("Congratulations, %s!", $name);
+            return;
         }
     }
+    line("Congratulations, %s!", $name);
 }
