6620bca60ee2b352d39476d61c060c523c154ae1
ArtemRyz <RyzArtem@yandex.ru>
2023-07-26 14:47:27 +0500

    refactoring Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -2,48 +2,31 @@
 
 namespace BrainGames\Even;
 
-use function cli\line;
-use function cli\prompt;
-use function BrainGames\Cli\greeting;
+use function BrainGames\Engine\runGame;
+
+const GAMEINFO = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+
 
 function isEven(int $num)
 {
     if ($num % 2 === 0) {
-        return true;
+        return 'yes';
     } else {
-        return false;
+        return 'no';
     }
 }
 
 function runEven()
 {
-    $name = greeting();
-    line("Answer 'yes' if the number is even, otherwise answer 'no'.");
-    for ($i = 0; $i < 3; $i++) {
-        $question = rand(1, 100);
-        $checkEven = isEven($question);
-        line('Question: %s', $question);
-        $answer = prompt('Your answer');
-        if (($answer !== 'yes') && ($answer !== 'no')) {
-            line("%s is wrong answer ;(. Correct answer was 'yes' or 'no'.", $answer);
-            line("Let's try again, %s!", $name);
-            break;
-        }
-        if ($checkEven === true && $answer === 'yes') {
-            line('Correct!');
-        } elseif ($checkEven === false && $answer === 'no') {
-            line('Correct!');
-        } elseif ($checkEven === true && $answer === 'no') {
-            line("'no' is wrong answer ;(. Correct answer was 'yes'.");
-            line("Let's try again, %s!", $name);
-            break;
-        } elseif ($checkEven === false && $answer === 'yes') {
-            line("'yes' is wrong answer ;(. Correct answer was 'no'.");
-            line("Let's try again, %s!", $name);
-            break;
-        }
-        if ($i === 2) {
-            line("Congratulations, %s!", $name);
-        }
+    $answer = [];
+    $question = [];
+    $round = 3;
+    $i = 0;
+    while ($i < $round) {
+        $randomNum = rand(1, 100);
+        $question[] = $randomNum;
+        $answer[] = isEven($randomNum);
+        $i++;
     }
+    runGame(GAMEINFO, $question, $answer);
 }
