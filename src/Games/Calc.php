11c15da14af4b74ac9c114f8bb4cd0e88e943948
ArtemRyz <RyzArtem@yandex.ru>
2023-08-23 15:30:16 +0500

    update calc

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -6,7 +6,7 @@ use Exception;
 
 use function BrainGames\Engine\runGame;
 
-use const BrainGames\Engine\ROUNDS;
+use const BrainGames\Engine\ROUNDS_COUNT;
 
 const RULE = 'What is the result of the expression?';
 
@@ -15,11 +15,11 @@ function runCalc(): void
     $gameData = [];
     $operators = ['+', '-', '*'];
 
-    for ($i = 0; $i < ROUNDS; $i += 1) {
+    for ($i = 0; $i < ROUNDS_COUNT; $i += 1) {
         $num1 = rand(1, 10);
         $num2 = rand(1, 10);
 
-        $operator = $operators[array_rand($operators, 1)];
+        $operator = $operators[array_rand($operators)];
         $question = "$num1 $operator $num2";
         $correctAnswer = (string)calcResult($num1, $operator, $num2);
         $gameData[] = [$question, $correctAnswer];
@@ -38,6 +38,6 @@ function calcResult(int $number1, string $operation, int $number2): int
         case "*":
             return $number1 * $number2;
         default:
-            throw new Exception("Incorrectly operation '{$operation}'");
+            throw new Exception("Incorrectly operation '$operation'");
     }
 }
